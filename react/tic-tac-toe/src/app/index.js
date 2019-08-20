c39b14458e9955d437402d84f575b5329a618f33
Elkin <elkin.bernal@wolox.co>
2019-08-20 06:17:50 -0500

    fixes PR

--- react/tic-tac-toe/src/app/index.js
+++ react/tic-tac-toe/src/app/index.js
@@ -1,32 +1,17 @@
-import React, { Component } from 'react';
-
-import MatchesService from '../services/MatchesService';
+import React from 'react';
 
 import Game from './screens/Game';
 import Matches from './screens/Matches';
 import '../scss/application.scss';
 import styles from './styles.module.scss';
 
-class App extends Component {
-  state = {
-    data: []
-  }
-
-  componentDidMount() {
-    MatchesService.getMatches().then(({ data }) => {
-      this.setState({ data });
-    });
-  }
-
-  render() {
-    const { data } = this.state;
-    return (
-      <div className={styles.container}>
-        <Game />
-        <Matches data={data} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className={styles.container}>
+      <Game />
+      <Matches />
+    </div>
+  );
 }
 
 export default App;
--- react/tic-tac-toe/src/app/screens/Matches/index.js
+++ react/tic-tac-toe/src/app/screens/Matches/index.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Spinner from 'react-spinkit';
+
+import MatchesService from '../../../services/MatchesService';
 
 import styles from './styles.module.scss';
 
-function Matches({ data }) {
-  return (
-    <div className={styles.table}>
-      <div className={styles.head}>
-        <div className={styles.headCell}>Player One</div>
-        <div className={styles.headCell}>Player Two</div>
-        <div className={styles.headCell}>Winner</div>
-      </div>
-      <div className={styles.body}>{data.length ? data.map(match => (
-        <div className={styles.match} key={match.id}>
-          <div className={styles.cell}>{match.player_one}</div>
-          <div className={styles.cell}>{match.player_two}</div>
-          <div className={styles.cell}>{match.winner}</div>
+class Matches extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    MatchesService.getMatches().then(({ data }) => {
+      this.setState({ data });
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.table}>
+        <div className={styles.head}>
+          <div className={styles.headCell}>Player One</div>
+          <div className={styles.headCell}>Player Two</div>
+          <div className={styles.headCell}>Winner</div>
         </div>
-      )) : <Spinner name="ball-scale-multiple" className={styles.spinner} />}
+        <div className={styles.body}>{data.length ? data.map(match => (
+          <div className={styles.match} key={match.id}>
+            <div className={styles.cell}>{match.player_one}</div>
+            <div className={styles.cell}>{match.player_two}</div>
+            <div className={styles.cell}>{match.winner}</div>
+          </div>
+        )) : <Spinner name="ball-scale-multiple" className={styles.spinner} />}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-Matches.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object)
-};
 export default Matches;
