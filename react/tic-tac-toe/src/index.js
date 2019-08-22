b9be269b249c92454aa6274f93af504b951462ad
Elkin <elkin.bernal@wolox.co>
2019-08-22 12:29:04 -0500

    applied router

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './config/i18n';
 import './scss/application.scss';
 
+import Login from './app/screens/Login';
 import App from './app';
 import { register } from './serviceWorker';
 import store from './redux/store';
+
 
 const render = () => {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
-        <App />
+        <Router>
+          <div>
+            <Route path="/" exact component={App} />
+            <Route path="/login" component={Login} />
+          </div>
+        </Router>
       </Provider>
     </AppContainer>,
     document.getElementById('root')
