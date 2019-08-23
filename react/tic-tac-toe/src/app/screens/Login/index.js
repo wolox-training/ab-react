a1c104e655e015f5adeb11529a871564e70a4a25
Elkin <elkin.bernal@wolox.co>
2019-08-23 06:56:46 -0500

    created login component

---
+++ react/tic-tac-toe/src/app/screens/Login/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Login" />
+      </form>
+    );
+  }
+}
+
+export default Login;
