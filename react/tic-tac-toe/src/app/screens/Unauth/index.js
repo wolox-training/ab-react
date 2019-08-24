bbbb50a54e73cc9b0b118a84a019292a00370dac
Elkin <elkin.bernal@wolox.co>
2019-08-24 11:47:11 -0500

    validate in unauth

--- react/tic-tac-toe/src/app/screens/Unauth/index.js
+++ react/tic-tac-toe/src/app/screens/Unauth/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import LocalStoreServie from '../../../services/LocalStoreService';
 
 import Login from './screens/Login';
 
 function Unauth({ match }) {
-  return (<>
-    <Route path={`${match.path}/login`} component={Login} />
-  </>);
+  return (
+    LocalStoreServie.getValue('token') ? <Redirect to="/" />
+      : <>
+        <Route path={`${match.path}/login`} component={Login} />
+      </>);
 }
+
+Unauth.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any)
+};
 
 export default Unauth;
